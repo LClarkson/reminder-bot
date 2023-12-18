df9a10c05ea7d75031c57871010c14e1c87a8abb
LClarkson <l.j.clarkson@gmail.com>
2023-12-17 21:01:01 -0800

    document formatting on index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable brace-style */
+/* eslint-disable indent */
 /* eslint-disable no-unused-vars */
 // Load environment variables from .env
 require('dotenv').config();
@@ -11,43 +13,42 @@ instance
 const { Client, Events, GatewayIntentBits, Partials } = require('discord.js');
 
 const client = new Client({
-	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
-	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
 /*
 Log message to confirm
 bot is online and logged in
 */
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
-
 
 /*
 Listen for user reactions
 to messages in server
 */
 client.on('messageReactionAdd', async (reaction, user) => {
-	// Check if the reaction is from the bot or another user
-	if (user.bot) return;
+  // Check if the reaction is from the bot or another user
+  if (user.bot) return;
 
-	try {
-		// Fetch the full message
-		const reactedMessage = await reaction.message.fetch();
+  try {
+    // Fetch the full message
+    const reactedMessage = await reaction.message.fetch();
 
-		// Extract information about the message
-		const author = reactedMessage.author.tag;
-		const content = reactedMessage.content;
+    // Extract information about the message
+    const author = reactedMessage.author.tag;
+    const content = reactedMessage.content;
 
-		// You can now use 'author' and 'content' as needed
-		console.log(reactedMessage);
-	}
-
-	catch (error) {
-		console.error('Error fetching message:', error);
-	}
-
+    console.log(reactedMessage);
+  } catch (error) {
+    console.error('Error fetching message:', error);
+  }
 });
 
 // Login to Discord API with Bot
