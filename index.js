e33ddde2a1ef57485269c63eb24509202a6dea8b
LClarkson <l.j.clarkson@gmail.com>
2023-12-26 15:05:07 -0600

    add nodemon

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+/* eslint-disable spaced-comment */
 /* eslint-disable brace-style */
 /* eslint-disable indent */
 /* eslint-disable no-unused-vars */
+
 // Load environment variables from .env
 require('dotenv').config();
+
+// Store discord API token in variable
 const token = process.env.DISCORD_TOKEN;
 
-/*
-Require the necessary discord.js
-classes and create a new client
-instance
-*/
+/*************** Require discord.js classes and create a new client instance ****************/
+
 const { Client, Events, GatewayIntentBits, Partials } = require('discord.js');
 
 const client = new Client({
@@ -21,18 +22,14 @@ const client = new Client({
   partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
-/*
-Log message to confirm
-bot is online and logged in
-*/
+/******************** Log message to confirm bot is online and logged in ********************/
+
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-/*
-Listen for user reactions
-to messages in server
-*/
+/********************** Listen for user reactions to messages in server *********************/
+
 client.on('messageReactionAdd', async (reaction, user) => {
   // Check if the reaction is from the bot or another user
   if (user.bot) return;
@@ -51,5 +48,5 @@ client.on('messageReactionAdd', async (reaction, user) => {
   }
 });
 
-// Login to Discord API with Bot
+/****************************** Log in to Discord API with Bot *****************************/
 client.login(token);
